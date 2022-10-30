bc22dd4c79ebc950cfe470a13efa5ecd521a644b
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2022-10-30 15:15:05 -0500

    search cities API

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,11 +1,30 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../citiesApi";
 
-const Search = () => {
+const Search = ({ onSearchChange }) => {
     const [search, setSearch] = useState(null);
+
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err)
+            )
+    }
 
     const handleOnChange = (searchData) => {
         setSearch(searchData);
+        onSearchChange(searchData);
     }
     return (
         <AsyncPaginate
@@ -13,6 +32,7 @@ const Search = () => {
             debounceTimeout={600}
             value={search}
             onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     )
 }
