b9bb97101745f2f5a6b7d3a076a944ac41bddde2
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2022-10-30 15:14:17 -0500

    cities GET

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import CurrentWeather from './components/current-weather/current-weather';
 import Search from './components/search/search';
 
 function App() {
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData)
+  }
   return (
     <div className="container">
-      <Search />
+      <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
     </div>
   );
 }
---
+++ src/citiesApi.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'c6ec5e75ccmshff4188ae1fbdbbcp1ed074jsn55e5d8d203f7',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
