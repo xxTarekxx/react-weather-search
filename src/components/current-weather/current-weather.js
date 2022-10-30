449b191d845935298d7a296d495296a24dada4fe
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2022-10-30 15:14:51 -0500

    current weather API

---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,14 @@
+import "./current-weather.css"
+
+const CurrentWeather = () => {
+    return (
+        <div className="weather">
+            <div className="top">
+                <p className="city">Egypt</p>
+                <p className="weather-description">Sunny</p>
+            </div>
+            <img src="" alt="weather" className="weather-icon" />
+        </div>
+    )
+}
+export default CurrentWeather;
