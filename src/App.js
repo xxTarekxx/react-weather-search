ca49ca835864bc3f93e49dfca20da96059638b29
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2022-11-07 21:22:40 -0600

    hooks for weather data added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useState } from 'react';
 import './App.css';
+import { WEATHER_API_KEY, WEATHER_API_URL } from './citiesApi';
 import CurrentWeather from './components/current-weather/current-weather';
 import Search from './components/search/search';
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecase, setForecast] = useState(null);
+
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData)
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&&appid=${WEATHER_API_KEY}`);
+    const foreCastFetch = fetch(`${WEATHER_API_URL}/forecast?lat={lat}&lon={lon}&appid=${WEATHER_API_KEY}`);
+
+    console.log(searchData);
+
+    Promise.all([currentWeatherFetch, foreCastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+
   }
   return (
     <div className="container">
