57c2a1473d9cb6632e8b3a08205624db158b9573
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2022-11-07 21:37:13 -0600

    reverting async

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -5,21 +5,21 @@ import { GEO_API_URL, geoApiOptions } from "../../citiesApi";
 const Search = ({ onSearchChange }) => {
     const [search, setSearch] = useState(null);
 
-    const loadOptions = async (inputValue) => {
-        try {
-            const response = await fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions);
-            const response_1 = await response.json();
-            return {
-                options: response_1.data.map((city) => {
-                    return {
-                        value: `${city.latitude} ${city.longitude}`,
-                        label: `${city.name}, ${city.countryCode}`,
-                    };
-                })
-            };
-        } catch (err) {
-            return console.error(err);
-        }
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err)
+            )
     }
 
     const handleOnChange = (searchData) => {
