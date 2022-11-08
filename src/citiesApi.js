4c9fd451e6b8fb11fe5e9f8c84e8a0f81510eee7
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2022-11-07 21:23:04 -0600

    cities API added

--- src/citiesApi.js
+++ src/citiesApi.js
@@ -8,3 +8,7 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "0f86c0f97f4a62686b4859344ba72ac1";
+
