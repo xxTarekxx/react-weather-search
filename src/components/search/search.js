9e59a5cccb8f016bce71eae4b8877e26cf7f37f2
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2022-11-13 18:54:31 -0600

    formatting with prettier

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -6,35 +6,37 @@ const Search = ({ onSearchChange }) => {
     const [search, setSearch] = useState(null);
 
     const loadOptions = (inputValue) => {
-        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
-            .then(response => response.json())
-            .then(response => {
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+            geoApiOptions,
+        )
+            .then((response) => response.json())
+            .then((response) => {
                 return {
                     options: response.data.map((city) => {
                         return {
                             value: `${city.latitude} ${city.longitude}`,
                             label: `${city.name}, ${city.countryCode}`,
-                        }
-                    })
-                }
+                        };
+                    }),
+                };
             })
-            .catch(err => console.error(err)
-            )
-    }
+            .catch((err) => console.error(err));
+    };
 
     const handleOnChange = (searchData) => {
         setSearch(searchData);
         onSearchChange(searchData);
-    }
+    };
     return (
         <AsyncPaginate
-            placeholder="City Search"
+            placeholder='City Search'
             debounceTimeout={600}
             value={search}
             onChange={handleOnChange}
             loadOptions={loadOptions}
         />
-    )
-}
+    );
+};
 
 export default Search;
